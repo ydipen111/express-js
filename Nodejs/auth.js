639d7c267b8f6deeaef9323f7971582f3a556edf
dipen <mgrd08716@gmail.com>
2023-08-13 16:09:33 +0545

    addiing file

---
+++ Nodejs/auth.js
@@ -0,0 +1,11 @@
+module.exports={
+    "login":login,
+    "signup":signup
+}
+
+function login(){
+    console.log("Login succesfully");
+}
+function signup(){
+    console.log("Signup succesfully")
+ }
---
+++ Nodejs/index.js
@@ -0,0 +1,9 @@
+const auth = require("./auth")
+// import auth form './auth'
+    
+auth.signup();
+auth.login();
+
+const {signup, login} = require("./auth")
+signup()
+login()
